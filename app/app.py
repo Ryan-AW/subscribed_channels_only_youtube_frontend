89368da099664243e92c3925885539130b524053
Ryan-AW <ryanawhittock@outlook.com>
2025-05-23 19:16:00 +0100

    added sql database

--- app/app.py
+++ app/app.py
@@ -4,9 +4,15 @@ from flask import Flask
 from .routes import BLUEPRINTS
 from ._server_keys import ServerKey
 
+from .database import db, init_db
+
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = ServerKey.value
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///db.sqlite"
+
+init_db(app)
+
 
 for blueprint in BLUEPRINTS:
     app.register_blueprint(blueprint)
